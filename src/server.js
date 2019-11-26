f33d2ecf4dc56afbd0d4cd67161d1fa2a445928c
lucas.riccioppo <lucas.riccioppo@valecard.com.br>
2019-11-26 13:51:49 -0200

    add middleware.js

--- src/server.js
+++ src/server.js
@@ -2,18 +2,11 @@ require('dotenv/config');
 require('./database')
 const express = require('express')
 const app = express()
-const bodyParser = require('body-parser')
-const router = require('./routes/routes')
-const compression = require('compression')
-const cors = require('cors')
+const { addMiddlewares } = require('./utils/middleware')
 
 const SERVER_PORT = process.env.SERVER_PORT
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors())
-app.use(compression())
-app.use(router)
+addMiddlewares(app)
 
 app.listen(SERVER_PORT, function () {
   console.log(`Server running on port ${SERVER_PORT}!`)
---
+++ src/utils/middleware.js
@@ -0,0 +1,14 @@
+const bodyParser = require('body-parser')
+const router = require('../routes/routes')
+const compression = require('compression')
+const cors = require('cors')
+
+const addMiddlewares = (app) => {
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(cors())
+    app.use(compression())
+    app.use(router)
+}
+
+module.exports = { addMiddlewares }
